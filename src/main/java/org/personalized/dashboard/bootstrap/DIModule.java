2a9a27875e6351a0d524374d198bdf7350e2ff6b
sudan <ssudan16@gmail.com>
2015-04-05 19:28:36 +0530

    configuring guice for bootstrap

---
+++ src/main/java/org/personalized/dashboard/bootstrap/DIModule.java
@@ -0,0 +1,14 @@
+package org.personalized.dashboard.bootstrap;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by sudan on 5/4/15.
+ */
+public class DIModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
--- src/main/java/org/personalized/dashboard/controller/BookmarkController.java
+++ src/main/java/org/personalized/dashboard/controller/BookmarkController.java
@@ -1,7 +1,14 @@
 package org.personalized.dashboard.controller;
 
+import org.springframework.stereotype.Controller;
+
+import javax.ws.rs.Path;
+
 /**
  * Created by sudan on 3/4/15.
  */
+
+@Controller
+@Path("/bookmark")
 public class BookmarkController {
 }
