bb22f91028914d4defadab56c92091c9eea1824a
sudan <ssudan16@gmail.com>
2015-04-07 00:03:58 +0530

    bind interface to impl

--- src/main/java/org/personalized/dashboard/bootstrap/DIModule.java
+++ src/main/java/org/personalized/dashboard/bootstrap/DIModule.java
@@ -1,6 +1,10 @@
 package org.personalized.dashboard.bootstrap;
 
 import com.google.inject.AbstractModule;
+import org.personalized.dashboard.dao.api.*;
+import org.personalized.dashboard.dao.impl.*;
+import org.personalized.dashboard.service.api.*;
+import org.personalized.dashboard.service.impl.*;
 
 /**
  * Created by sudan on 5/4/15.
@@ -10,5 +14,16 @@ public class DIModule extends AbstractModule {
     @Override
     protected void configure() {
 
+        bind(BookmarkService.class).to(BookmarkServiceImpl.class);
+        bind(DiaryService.class).to(DiaryServiceImpl.class);
+        bind(NoteService.class).to(NoteServiceImpl.class);
+        bind(PinService.class).to(PinServiceImpl.class);
+        bind(TodoService.class).to(TodoServiceImpl.class);
+
+        bind(BookmarkDao.class).to(BookmarkDaoImpl.class);
+        bind(DiaryDao.class).to(DiaryDaoImpl.class);
+        bind(NoteDao.class).to(NoteDaoImpl.class);
+        bind(PinDao.class).to(PinDaoImpl.class);
+        bind(TodoDao.class).to(TodoDaoImpl.class);
     }
 }
