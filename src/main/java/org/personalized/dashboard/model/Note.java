3d6ca47ae9eb4126f4760efe61d0c682c946bccb
sudan <ssudan16@gmail.com>
2015-04-09 01:20:51 +0530

    Added note model with test case

--- src/main/java/org/personalized/dashboard/model/Note.java
+++ src/main/java/org/personalized/dashboard/model/Note.java
@@ -1,7 +1,79 @@
 package org.personalized.dashboard.model;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 /**
  * Created by sudan on 3/4/15.
  */
 public class Note {
+
+    private String noteId;
+    private String title;
+    private String note;
+    private Long createdOn;
+    private Long modifiedAt;
+    private String userId;
+
+    public Note(){
+
+    }
+
+    public Note(String noteId, String title, String note, String userId){
+        this.noteId = noteId;
+        this.title = title;
+        this.note = note;
+        this.userId = userId;
+    }
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(String noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getCreatedOn() {
+        return createdOn;
+    }
+
+    public Long getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this)
+                .append("noteId", noteId)
+                .append("title", title)
+                .append("note", note)
+                .append("userId", userId)
+                .append("createdOn", createdOn)
+                .append("modifiedAt", modifiedAt)
+                .toString();
+    }
 }
