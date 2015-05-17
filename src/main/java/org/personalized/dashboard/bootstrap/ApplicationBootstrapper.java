8dc6603732a14777c0ecac4176c5cbb9845cf7f1
sudan <ssudan16@gmail.com>
2015-05-17 22:37:10 +0530

    used slf4j for logging and shutdown app if init fails

--- src/main/java/org/personalized/dashboard/bootstrap/ApplicationBootstrapper.java
+++ src/main/java/org/personalized/dashboard/bootstrap/ApplicationBootstrapper.java
@@ -7,20 +7,27 @@ import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by sudan on 5/4/15.
  */
 public class ApplicationBootstrapper extends GuiceServletContextListener{
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ApplicationBootstrapper.class);
     @Override
     protected Injector getInjector() {
 
         try {
             ConfigManager.init();
             MongoBootstrap.init();
+            MongoBootstrap.getMongoDatabase().runCommand(new Document("ping", 1));
+            LOGGER.info("Application initialization has been successful");
         }
         catch (Exception e){
+            LOGGER.error("Application initialization failed with exception  ", e);
             System.exit(1);
         }
         return Guice.createInjector(new ServletModule(){
