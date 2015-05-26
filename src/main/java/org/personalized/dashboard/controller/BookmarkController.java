05d00c88a0573ed735dde67a4d7c77bce4fd1f17
sudan <ssudan16@gmail.com>
2015-05-26 23:40:57 +0530

    created a post service for bookmark creation

--- src/main/java/org/personalized/dashboard/controller/BookmarkController.java
+++ src/main/java/org/personalized/dashboard/controller/BookmarkController.java
@@ -1,11 +1,17 @@
 package org.personalized.dashboard.controller;
 
 import com.google.inject.Inject;
+import org.personalized.dashboard.model.Bookmark;
 import org.personalized.dashboard.service.api.BookmarkService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by sudan on 3/4/15.
@@ -13,7 +19,7 @@ import javax.ws.rs.Path;
 
 @Controller
 @Scope("request")
-@Path("/bookmark")
+@Path("/bookmarks")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
@@ -21,5 +27,14 @@ public class BookmarkController {
     @Inject
     public BookmarkController(BookmarkService bookmarkService){
         this.bookmarkService = bookmarkService;
+    }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createBookmark(Bookmark bookmark) {
+        bookmarkService.createBookmark(bookmark);
+        return Response.status(Response.Status.CREATED).build();
     }
 }
