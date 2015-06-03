e91eb5dd202165ea4573ebf63f61f587c71c8b0f
sudan <ssudan16@gmail.com>
2015-06-04 00:54:08 +0530

    added pin test case

--- src/test/java/org/personalized/dashboard/utils/validator/PinValidationTest.java
+++ src/test/java/org/personalized/dashboard/utils/validator/PinValidationTest.java
@@ -1,8 +1,13 @@
 package org.personalized.dashboard.utils.validator;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.personalized.dashboard.model.Pin;
+import org.personalized.dashboard.utils.Constants;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 /**
  * Created by sudan on 30/5/15.
@@ -19,56 +24,65 @@ public class PinValidationTest {
 
     @Test
     public void testPinValidation() {
-        // TODO this will be revisited again once the validation is refactored completely
-
-        /*
-        Pin pin = new Pin("PINE7W7VETWZAKE0", "pin","desc","http://www.google.com");
-
+        Pin pin = new Pin("PIN123456789", "", "", "");
         List<ErrorEntity> errorEntities = pinValidationService.validate(pin);
+        Assert.assertEquals("Error count is 4", 4, errorEntities.size());
+
+        Assert.assertEquals("Error 1 name matches", ErrorCodes.EMPTY_FIELD.name(), errorEntities.get(0).getName());
+        Assert.assertEquals("Error 1 description matches", "name cannot be empty", errorEntities.get(0).getDescription());
+        Assert.assertEquals("Error 1 field matches", "name", errorEntities.get(0).getField());
+
+        Assert.assertEquals("Error 2 name matches", ErrorCodes.EMPTY_FIELD.name(), errorEntities.get(1).getName());
+        Assert.assertEquals("Error 2 description matches", "description cannot be empty", errorEntities.get(1).getDescription());
+        Assert.assertEquals("Error 2 field matches", "description", errorEntities.get(1).getField());
+
+        Assert.assertEquals("Error 3 name matches", ErrorCodes.EMPTY_FIELD.name(), errorEntities.get(2).getName());
+        Assert.assertEquals("Error 3 description matches", "image_url cannot be empty", errorEntities.get(2).getDescription());
+        Assert.assertEquals("Error 3 field matches", "image_url", errorEntities.get(2).getField());
+
+        Assert.assertEquals("Error 4 name matches", ErrorCodes.INVALID_URL.name(), errorEntities.get(3).getName());
+        Assert.assertEquals("Error 4 description matches", "Invalid URL format", errorEntities.get(3).getDescription());
+        Assert.assertEquals("Error 4 field matches", "image_url", errorEntities.get(3).getField());
+
+        pin = new Pin("PIN123456789", "name", "desc", "http://www.google.com");
+        errorEntities = pinValidationService.validate(pin);
         Assert.assertEquals("Error count is 0", 0, errorEntities.size());
 
-        StringBuilder invalidUrl = new StringBuilder();
+        StringBuilder invalidImageUrl = new StringBuilder();
         for(int i = 0; i < Constants.URL_MAX_LENGTH + 1; i++) {
-            invalidUrl.append("a");
+            invalidImageUrl.append("a");
         }
 
-        pin = new Pin("PINE7W7VETWZAKE0", "pin","desc", invalidUrl.toString());
-        errorEntities = pinValidationService.validate(pin);
-        Assert.assertEquals("Error count is 2", 2, errorEntities.size());
-
-        Assert.assertEquals("Error 1 is INVALID_URL", ErrorCodes.INVALID_URL.name(), errorEntities.get(0).getName());
-        Assert.assertEquals("Error description matches", ErrorCodes.INVALID_URL.getDescription(), errorEntities.get(0).getDescription());
-
-        Assert.assertEquals("Error 2 is MAX_PIN_URL_LENGTH_EXCEEDED", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(1).getName());
-        Assert.assertEquals("Error description matches", "url length cannot exceed 300 characters", errorEntities.get(1).getDescription());
-
-        StringBuilder invalidName  = new StringBuilder();
-        for(int i = 0; i < Constants.TITLE_MAX_LENGTH + 1; i++){
-            invalidName.append("a");
+        StringBuilder invalidName = new StringBuilder();
+        for(int i = 0; i < Constants.TITLE_MAX_LENGTH + 1; i++) {
+            invalidName.append("n");
         }
 
         StringBuilder invalidDescription = new StringBuilder();
         for(int i = 0; i < Constants.CONTENT_MAX_LENGTH + 1; i++) {
-            invalidDescription.append("b");
+            invalidDescription.append("c");
         }
 
-        pin = new Pin("PINE7W7VETWZAKE0", invalidName.toString(),invalidDescription.toString(), invalidUrl.toString());
-
+        pin = new Pin("PIN123456789", invalidName.toString(), invalidDescription.toString(), invalidImageUrl.toString());
         errorEntities = pinValidationService.validate(pin);
-        Assert.assertEquals("Error count is 2", 4, errorEntities.size());
-
-        Assert.assertEquals("Error 1 is INVALID_URL", ErrorCodes.INVALID_URL.name(), errorEntities.get(0).getName());
-        Assert.assertEquals("Error description matches", ErrorCodes.INVALID_URL.getDescription(), errorEntities.get(0).getDescription());
-
-        Assert.assertEquals("Error 2 is MAX_PIN_URL_LENGTH_EXCEEDED", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(1).getName());
-        Assert.assertEquals("Error description matches", "url length cannot exceed 300 characters", errorEntities.get(1).getDescription());
+        Assert.assertEquals("Error count is 4", 4, errorEntities.size());
 
 
-        Assert.assertEquals("Error 3 is MAX_PIN_NAME_LENGTH_EXCEEDED", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(2).getName());
-        Assert.assertEquals("Error description matches", "name length cannot exceed 50 characters", errorEntities.get(2).getDescription());
+        Assert.assertEquals("Error 1 name matches", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(0).getName());
+        Assert.assertEquals("Error 1 description matches", "name cannot exceed 50 characters", errorEntities.get(0).getDescription());
+        Assert.assertEquals("Error 1 field matches", "name", errorEntities.get(0).getField());
 
-        Assert.assertEquals("Error 2 is MAX_PIN_DESC_LENGTH_EXCEEDED", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(3).getName());
-        Assert.assertEquals("Error description matches", "description length cannot exceed 1,000 characters", errorEntities.get(3).getDescription());
-        */
+        Assert.assertEquals("Error 2 name matches", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(1).getName());
+        Assert.assertEquals("Error 2 description matches", "description cannot exceed 1,000 characters", errorEntities.get(1).getDescription());
+        Assert.assertEquals("Error 2 field matches", "description", errorEntities.get(1).getField());
+
+        Assert.assertEquals("Error 3 name matches", ErrorCodes.LENGTH_EXCEEDED.name(), errorEntities.get(2).getName());
+        Assert.assertEquals("Error 3 description matches", "image_url cannot exceed 300 characters", errorEntities.get(2).getDescription());
+        Assert.assertEquals("Error 3 field matches", "image_url", errorEntities.get(2).getField());
+
+        Assert.assertEquals("Error 4 name matches", ErrorCodes.INVALID_URL.name(), errorEntities.get(3).getName());
+        Assert.assertEquals("Error 4 description matches", "Invalid URL format", errorEntities.get(3).getDescription());
+        Assert.assertEquals("Error 4 field matches", "image_url", errorEntities.get(3).getField());
+
     }
 }
