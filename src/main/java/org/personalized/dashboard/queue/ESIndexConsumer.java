7d4814155aa420c80fdc8b79a80633cb98d529ff
sudan <ssudan16@gmail.com>
2015-07-05 00:48:02 +0530

    pushing to consumer

--- src/main/java/org/personalized/dashboard/queue/ESIndexConsumer.java
+++ src/main/java/org/personalized/dashboard/queue/ESIndexConsumer.java
@@ -1,15 +1,20 @@
 package org.personalized.dashboard.queue;
 
 import org.personalized.dashboard.bootstrap.QueueBootstrap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 /**
  * Created by sudan on 4/7/15.
  */
 public class ESIndexConsumer implements MessageListener {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ESIndexConsumer.class);
 
     public ESIndexConsumer() throws JMSException {
         QueueBootstrap.getInstance().getMessageConsumer().setMessageListener(this);
@@ -17,5 +22,13 @@ public class ESIndexConsumer implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
+        if (message != null) {
+            TextMessage textMessage = (TextMessage) message;
+            try {
+                String payload = textMessage.getText();
+            } catch (JMSException e) {
+                LOGGER.error("Error in consuming message ", textMessage);
+            }
+        }
     }
 }
--- src/main/java/org/personalized/dashboard/queue/PinESIndexProducer.java
+++ src/main/java/org/personalized/dashboard/queue/PinESIndexProducer.java
@@ -1,16 +1,50 @@
 package org.personalized.dashboard.queue;
 
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.personalized.dashboard.bootstrap.QueueBootstrap;
 import org.personalized.dashboard.model.EntityType;
 import org.personalized.dashboard.model.OperationType;
 import org.personalized.dashboard.model.Pin;
+import org.personalized.dashboard.utils.FieldKeys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import java.util.Map;
 
 /**
  * Created by sudan on 4/7/15.
  */
 public class PinESIndexProducer implements ESIndexProducer<Pin> {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(PinESIndexProducer.class);
+
     @Override
     public void enqueue(Pin obj, EntityType entityType, OperationType operationType, String entityId) {
 
+
+        Gson gson = new GsonBuilder().create();
+        Map<String, String> payload = Maps.newHashMap();
+        payload.put(FieldKeys.ES_ID, entityId);
+        payload.put(FieldKeys.ES_ENTITY_TYPE, entityType.name());
+        payload.put(FieldKeys.ES_OP_TYPE, operationType.name());
+        if (operationType != OperationType.DELETE) {
+            payload.put(FieldKeys.ES_TITLE, obj.getName());
+            payload.put(FieldKeys.ES_EXT_URL, obj.getImageUrl());
+            payload.put(FieldKeys.ES_DESCRIPTION, obj.getDescription());
+        }
+        String text = gson.toJson(payload);
+        MessageProducer producer = QueueBootstrap.getInstance().getMessageProducer();
+        Session session = QueueBootstrap.getInstance().getSession();
+
+        try {
+            producer.send(session.createTextMessage(text));
+        } catch (JMSException e) {
+            LOGGER.error("Exception in enqueueing ", obj, "with ", entityId, " for ", operationType, " for es indexing");
+        }
     }
 }
