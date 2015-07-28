fa2836cd743a9bec1d81a0f1b5fcf00ee488b7a8
sudan <ssudan16@gmail.com>
2015-07-29 00:49:43 +0530

    minor changes

--- src/main/webapp/custom/js/bookmark.js
+++ src/main/webapp/custom/js/bookmark.js
@@ -1,8 +1,26 @@
-(function($, window, document, undefined){
+(function($, window, document){
 
     "use strict"
 
-    
+    var Bookmark = Backbone.Model.extend({
+    	urlRoot: '/iPersonal/dashboard/bookmarks',
 
+    	validate: function(attributes) {
+
+    		if(!attributes.name) {
+    			return "name cannot be empty";
+    		}
+
+    		if(!attributes.description) {
+    			return "description cannot be empty";
+    		}
+
+    		if(!attributes.url) {
+    			return "url cannot be empty";
+    		}
+    	}
+    });
+
+    window.bookmarks = new Bookmark();
 
 })(jQuery, window, document);
