63e583495b8de1abb13dd0c656c5152571913cf8
sudan <ssudan16@gmail.com>
2015-08-15 20:51:47 +0530

    beautify pin.js

--- src/main/webapp/custom/js/pin.js
+++ src/main/webapp/custom/js/pin.js
@@ -1,23 +1,27 @@
-(function($, window, document){
+(function($, window, document) {
 
     "use strict"
 
     var PIN_URL_ROOT = '/iPersonal/dashboard/pins';
 
     var Pin = Base.extend({
-    	urlRoot: PIN_URL_ROOT,
-        
+        urlRoot: PIN_URL_ROOT,
+
         initialize: function() {
             this.mandatory = {
-                'name': true, 'imageUrl': true, 'description': true
-            },
-            this.maxLength = {
-                'name': 50, 'imageUrl': 150, 'description': 1000
-            },
-            this.formAttributes = ['name', 'imageUrl', 'description']
+                    'name': true,
+                    'imageUrl': true,
+                    'description': true
+                },
+                this.maxLength = {
+                    'name': 50,
+                    'imageUrl': 150,
+                    'description': 1000
+                },
+                this.formAttributes = ['name', 'imageUrl', 'description']
         }
 
-    }); 
+    });
 
     var Pins = Backbone.Collection.extend({
         model: Pin
@@ -30,7 +34,7 @@
         upsertTemplate: $('#pin-upsert-template').html(),
         displayTemplate: $('#pin-display-template').html(),
 
-        events : {
+        events: {
             'click #pin-submit': 'upsertPin',
             'click #pin-cancel': 'resetValues',
             'click #pin-tag-img': 'displayTagSelection',
@@ -48,8 +52,8 @@
 
         prepareVariables: function() {
             this.tagImage = $('#pin-tag-img');
-            this.searchTag =  $('#pin-tag');
-            this.saveForm =  $('#pin-form');
+            this.searchTag = $('#pin-tag');
+            this.saveForm = $('#pin-form');
 
         },
 
@@ -66,7 +70,10 @@
             var entityId = self.saveForm.find('.entityId').html();
 
             if (entityId) {
-                self.model = new Pin({ id : entityId, pinId : entityId });
+                self.model = new Pin({
+                    id: entityId,
+                    pinId: entityId
+                });
             } else {
                 self.model = new Pin();
             }
@@ -77,7 +84,7 @@
                 description: self.saveForm.find('[name=description]').val()
             });
 
-            self.model.on('invalid', function(model , error) {
+            self.model.on('invalid', function(model, error) {
                 self.renderErrors(error);
             });
 
@@ -87,7 +94,7 @@
             });
 
             if (result) {
-                result.complete(function(response){
+                result.complete(function(response) {
                     if (response.status != 201 && response.status != 200) {
                         var errors = self.buildErrorObject(response, self);
                         self.renderErrors(errors);
@@ -105,7 +112,7 @@
                         } else {
                             self.postCreation(entityId, "PIN", self.model.get('name'), 0, tags);
                             self.model.set({
-                                id : entityId,
+                                id: entityId,
                                 'modifiedAt': Math.floor(Date.now()),
                                 'tags': tags
                             });
@@ -128,35 +135,48 @@
                 e.preventDefault();
             }
 
-            if (this.collection.length  == parseInt(entityCountModel.attributes.pins)) {
+            if (this.collection.length == parseInt(entityCountModel.attributes.pins)) {
                 var entityList = this.buildEntityList();
                 backboneGlobalObj.trigger('entity:displaylist', entityList);
                 return;
             }
-             
-            this.model = new Pin();   
-            this.model.fetch({data: {offset: this.collection.length, limit : 20} }).complete(function(response){
+
+            this.model = new Pin();
+            this.model.fetch({
+                data: {
+                    offset: this.collection.length,
+                    limit: 20
+                }
+            }).complete(function(response) {
                 if (response.status == 200) {
                     var pins = JSON.parse(response.responseText)['pin'];
                     if (pins instanceof Array) {
                         for (var index in pins) {
                             var pin = new Pin(pins[index])
-                            pin.set({ id : pins[index]['pinId']});
+                            pin.set({
+                                id: pins[index]['pinId']
+                            });
                             if (pin.attributes.tags && !(pin.attributes.tags instanceof Array)) {
                                 var tags = [];
                                 tags.push(pin.attributes.tags);
-                                pin.set({ 'tags': tags });
+                                pin.set({
+                                    'tags': tags
+                                });
                             }
 
                             self.collection.push(pin);
                         }
                     } else if (pins) {
                         var pin = new Pin(pins);
-                        pin.set({ id : pins['pinId']});
+                        pin.set({
+                            id: pins['pinId']
+                        });
                         if (pin.attributes.tags && !(pin.attributes.tags instanceof Array)) {
                             var tags = [];
                             tags.push(pin.attributes.tags);
-                            pin.set({ 'tags': tags });
+                            pin.set({
+                                'tags': tags
+                            });
                         }
                         self.collection.push(pin);
                     }
@@ -173,10 +193,10 @@
             for (var i = 0; i < this.collection.length; i++) {
                 var description = this.collection.models[i].attributes.description;
                 var entity = {
-                    'entityId' : this.collection.models[i].attributes.id,
-                    'entityTitle' : this.collection.models[i].attributes.name,
+                    'entityId': this.collection.models[i].attributes.id,
+                    'entityTitle': this.collection.models[i].attributes.name,
                     'url': this.collection.models[i].attributes.imageUrl,
-                    'entitySummary': description ? description.substring(0,100) : description,
+                    'entitySummary': description ? description.substring(0, 100) : description,
                     'entityType': 'pin',
                     'modifiedAt': this.collection.models[i].attributes.modifiedAt,
                 };
@@ -193,6 +213,8 @@
         }
     });
 
-    window.pinView = new PinView({ collection: new Pins() });
+    window.pinView = new PinView({
+        collection: new Pins()
+    });
 
 })(jQuery, window, document);
