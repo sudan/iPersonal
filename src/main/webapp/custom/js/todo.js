c7165f77bb91fcd7c790ccf35b2d45e64dd84402
sudan <ssudan16@gmail.com>
2015-08-27 13:29:33 +0530

    addng color variations

--- src/main/webapp/custom/js/todo.js
+++ src/main/webapp/custom/js/todo.js
@@ -6,18 +6,35 @@
 
     });
 
+    var Task = Base.extend({
+        initialize: function() {
+            this.mandatory = {
+                'name': true,
+                'task': true
+            },
+            this.maxLength = {
+                'name': 50,
+                'task': 300
+            },
+            this.formAttributes = ['name', 'task'];
+        }
+    });
+
     var TodoView = BaseView.extend({
 
         entityType: 'TODO',
         el: $('#todo-wrapper'),
         upsertTemplate: $('#todo-upsert-template').html(),
         displayTemplate: $('#todo-display-template').html(),
+        taskTemplate: $('#task-display-template').html(),
 
         events: {
             'click #todo-submit': 'upsertTodo',
             'click #todo-cancel': 'resetValues',
             'click #todo-tag-img': 'displayTagSelection',
-            'click .add-task' : 'showTaskForm'
+            'click .add-task' : 'showTaskForm',
+            'click #task-add-button': 'addTask',
+            'click #task-cancel-button': 'cancelTask'
         },
 
         showTaskForm: function(e) {
@@ -36,7 +53,8 @@
         },
 
         prepareVariables: function() {
-
+            this.saveForm = $('#todo-form');
+            this.tasks =  $('#tasks');
         },
 
         initializeUpdateForm: function() {
@@ -57,7 +75,40 @@
 
         getDeletableModel: function(id) {
 
-        }
+        },
+
+        addTask: function(e) {
+            e.preventDefault();
+
+            var percentCompletion = this.$el.find('#percent-completion').val() ? this.$el.find('#percent-completion').val() : 0;
+            this.model = new Task({
+                name : this.$el.find('[name=name]').val(),
+                task : this.$el.find('[name=task]').val(),
+                priority: this.$el.find('[name=priority]:checked').val().toLowerCase(),
+                percentCompletion: percentCompletion
+            });
+
+            var errors = this.model.validate(this.model.attributes);
+            if (errors) {
+                this.renderErrors(errors);
+                return;
+            }
+
+            this.$el.find('#task-form').addClass('invisible').fadeOut();
+            this.$el.find('.add-task').removeClass('invisible').fadeIn();
+
+            var template = _.template(this.taskTemplate);
+            this.tasks.append(template(this.model.toJSON()));
+        },
+
+        cancelTask: function(e) {
+            e.preventDefault();
+
+            this.$el.find('[name=name]').val('');
+            this.$el.find('[name=task]').val('');
+            this.$el.find('#task-form').addClass('invisible').fadeOut();
+            this.$el.find('.add-task').removeClass('invisible').fadeIn();
+        },
 
     });
 
